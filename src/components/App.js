56052f4cd797a32ac4762e11ded083565565e417
Mike Duin <michael.s.duin@gmail.com>
2019-01-16 15:22:19 -0800

    use async to handle response from axios call

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID 0b4ca2c911b76d8eb6ed3f444903df06df4f8b02e76c659357e7ea89625fad92'
       }
-    });
+    })
+
+    console.log(response.data.results);
   }
 
   render () {
